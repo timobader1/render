94ca9e71295538dc695599e1d986bd1ea533729f
Timo Bader <ti261bad@htwg-konstanz.de>
2024-01-24 20:19:31 +0100

    großschreibung geändert

--- api/controllers/account/view-account-overview.js
+++ api/controllers/account/view-account-overview.js
@@ -1,7 +1,7 @@
 module.exports = {
   friendlyName: 'View account overview',
   description: 'Display "Account Overview" page.',
-  exits: {success: {viewTemplatePath: 'pages/account/account-overview'}},
+  exits: {success: {viewTemplatePath: 'pages/Account/account-overview'}},
   fn: async function () {
     return {
       stripePublishableKey: sails.config.custom.enableBillingFeatures
